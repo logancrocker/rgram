998fabd4baa39d26042193da997ff4dfb21e8b8d
Logan Crocker <lcroc001@ucr.edu>
2018-10-31 16:13:17 -0800

    login page created, working on logic, register etc.

---
+++ source/app/src/main/java/com/rgram/rgram/ActivityRegister.java
@@ -0,0 +1,13 @@
+package com.rgram.rgram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityRegister extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+    }
+}
--- source/app/src/main/java/com/rgram/rgram/MainActivity.java
+++ source/app/src/main/java/com/rgram/rgram/MainActivity.java
@@ -2,6 +2,9 @@ package com.rgram.rgram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String dummyEmail = "email@email.com";
+        final String dummyPass = "pass";
+
+        //define the buttons
+        final Button loginButton = findViewById(R.id.button_login);
+        final Button registerButton = findViewById(R.id.button_register);
+        final EditText emailEntry = findViewById(R.id.email);
+        final EditText passwordEntry = findViewById(R.id.password);
+
+        //define action for login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEntry.getText().toString();
+                String password = passwordEntry.getText().toString();
+                if (email == dummyEmail && password == dummyPass)
+                {
+                    setContentView(R.layout.activity_feed);
+                }
+                else
+                {
+
+                }
+
+            }
+        });
     }
 }
